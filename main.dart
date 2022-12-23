5697f1431da543bc4d0724e5f934391a59329bc7
Yasith Nawodya <yasithnawodya8@gmail.com>
2022-12-23 22:52:56 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+  var name = 'Hasith';
+  var year = 2002;
+  
+  if (year >= 2000) {
+     print(name+' Your Access Granted');
+} else if (year >= 2004) {
+     print(name+' Get Permission to Access');
+}
+  else{
+    print(name+' Access Denied');
+  }
+}
