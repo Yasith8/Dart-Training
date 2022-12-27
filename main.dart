29554c81e86850fd791bbdf661f30d68ccc304bd
Yasith Nawodya <yasithnawodya8@gmail.com>
2022-12-27 22:57:18 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,7 +4,13 @@ void main() {
   
   if (year >= 2000) {
      print(name+' Your Access Granted');
-} else if (year >= 2004) {
+} else if (year >= 2006) {
+     print(name+' "wait for instruction");
+}
+  } else if (year >= 2008) {
+     print(name+' Get Permission to Access');
+}
+} else if (year >= 2010) {
      print(name+' Get Permission to Access');
 }
   else{
